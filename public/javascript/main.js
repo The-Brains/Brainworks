cb92698823a17fcfcc525ab4ece8a750a5395435
Tobias Kölpin <t.koelpin@web.de>
2015-03-18 08:56:26 +0100

    My own layout which works

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -10,3 +10,16 @@ brainworks.config(function($routeProvider) {
   .when('/about', { templateUrl: 'html/about.html' })
   .otherwise({ redirectTo: '/' });
 });
+
+// brainworks.controller('menuController', function($scope, $routeProvider){
+////  $routeProvider
+////  .when('/').then($scope.active = [{'home': "class=\"nav active\""},
+////                                   {'signin': "class=\"nav\""},
+////                                   {'about': "class=\"nav\""}])
+////  .when('/signin').then($scope.active = [{'home': "class=\"nav\""},
+////                                         {'signin': "class=\"nav active\""},
+////                                         {'about': "class=\"nav\""}])
+////  .when('/about').then($scope.active = [{'home': "class=\"nav\""},
+////                                        {'signin': "class=\"nav\""},
+////                                        {'about': "class=\"nav active\""}]);
+//}); 
