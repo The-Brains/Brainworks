20e8397dba6f9b818b5daae9a93a8cb16946b6be
Tobias Kölpin <t.koelpin@web.de>
2015-04-28 08:42:37 +0200

    Merge

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,25 +1,56 @@
 /**
- * New node file
+ * Main-Module der Applikation. Sie dient zum Konfigurieren und Initialisieren.
+ * 
+ * @author Dennis Stumm
  */
-var brainworks = angular.module('brainworks', ['ngAnimate', 'ngRoute']);
+var brainworks = angular.module('brainworks', ['ui.router', 'diagrams']);
 
-brainworks.config(function($routeProvider) {
-  $routeProvider
-  .when('/', { templateUrl: 'html/home.html' })
-  .when('/signin', { templateUrl: 'html/signin.html' })
-  .when('/about', { templateUrl: 'html/about.html' })
-  .otherwise({ redirectTo: '/' });
+brainworks.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home'
+    })
+    .state('sign_in', {
+      url: '/sign_in',
+      templateUrl: '/sign_in'
+    })
+    .state('profile', {
+      abstract: true,
+      template: '<ui-view/>'
+    })
+    .state('profile.diagrams', {
+      url: '/diagrams',
+      templateUrl: '/diagrams',
+      controller: 'diagramsCtrl'
+    })
+    .state('profile.settings', {
+      url: '/settings',
+      templateUrl: '/settings'
+    })
+    // TODO hier und beim einloggen muss ein redirect durchgefuehrt werden
+    .state('profile.logout', {
+      url: '/logout',
+      templateUrl: '/home'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: '/about'
+    });
+  $urlRouterProvider.otherwise('home');
 });
-
-// brainworks.controller('menuController', function($scope, $routeProvider){
-////  $routeProvider
-////  .when('/').then($scope.active = [{'home': "class=\"nav active\""},
-////                                   {'signin': "class=\"nav\""},
-////                                   {'about': "class=\"nav\""}])
-////  .when('/signin').then($scope.active = [{'home': "class=\"nav\""},
-////                                         {'signin': "class=\"nav active\""},
-////                                         {'about': "class=\"nav\""}])
-////  .when('/about').then($scope.active = [{'home': "class=\"nav\""},
-////                                        {'signin': "class=\"nav\""},
-////                                        {'about': "class=\"nav active\""}]);
-//}); 
+brainworks.controller('brainworksCtrl', function($scope) {
+  $scope.signed_in = true;
+});
+brainworks.directive('navItem', function($location) {
+  return {
+    restrict: 'E',
+    replace: true,
+    transclude: true,
+    scope: {
+      page: '@page',
+      title: '@title'
+    },
+    template: '<li ui-sref-active="active"><a ui-sref="{{page}}">{{title}}</a></li>'
+  };
+});
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,37 @@
-/*
- * GET home page.
- */
+var express = require('express');
+var router = express.Router();
 
-exports.index = function(req, res) {
+router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'Brainworks',
-    button_home: 'Home',
-    button_signin: 'Sign in',  
-    button_about: 'About'  
+    home: 'Startseite',
+    sign_in: 'Einloggen',
+    profile: 'Profil',
+    about: 'Über',
+    diagrams: 'Diagramme',
+    logout: 'Ausloggen',
+    settings: 'Einstellungen'
   });
-};
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home', {});
+});
+
+router.get('/sign_in', function(req, res, next) {
+  res.render('sign_in', {});
+});
+
+router.get('/diagrams', function(req, res, next) {
+  res.render('diagrams', {});
+});
+
+router.get('/settings', function(req, res, next) {
+  res.render('settings', {});
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {});
+});
+
+module.exports = router;
