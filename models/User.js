2d1b6d470fcc795e41950044998ead99ceb5273f
dennis95stumm <dstumm95@gmail.com>
2015-05-04 13:37:46 +0200

    Merge von Tobias

---
+++ models/User.js
@@ -0,0 +1,14 @@
+/**
+ * New node file
+ */
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+  forename: String,
+  surname: String,
+  username: String,
+  password: String,
+  email: String
+});
+
+mongoose.model('User', userSchema, 'user');
