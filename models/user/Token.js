20b6f7d1487b816ee72c99affb485d9b76999477
Dennis Stumm <dstumm95@gmail.com>
2015-05-20 16:13:50 +0200

    Changes

--- models/user/Token.js
+++ models/user/Token.js
@@ -7,8 +7,7 @@ var tokenSchema = new mongoose.Schema({
   createdAt: {type: Date, 'default': Date.now},
   revokedAt: {type: Date},
   token: {type: String},
-  userId: {type: mongoose.Schema.ObjectId, auto: false},
-  loggedIn: {type: Boolean}
+  userId: {type: mongoose.Schema.ObjectId, auto: false}
 });
 
 module.exports = mongoose.model('Token', tokenSchema);
