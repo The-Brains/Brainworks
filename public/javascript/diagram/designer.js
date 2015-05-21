9c37e5f1b9905fcf12a459d77fca542684a94368
KyleGass <tim.moenkemoeller@web.de>
2015-05-21 09:23:25 +0200

    Implementierung des Accordions (Dynamisch)

--- public/javascript/diagram/designer.js
+++ public/javascript/diagram/designer.js
@@ -1,4 +1,15 @@
-/**
- * New node file
- */
-angular.module('brainworks.diagram');
+angular.module('brainworks.diagram')
+.controller('AccordionCtrl', function ($scope) {
+  $scope.oneAtATime = true;
+
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+});
