81dff2bb5f39a2ad5899ed0458648e81ac255cae
Dennis Stumm <dstumm95@gmail.com>
2015-05-21 21:02:29 +0200

    Zwischenstand

--- models/diagrams/Relation.js
+++ models/diagrams/Relation.js
@@ -1,12 +1,19 @@
 /**
  * @author Dennis Stumm
  */
-var mongoose = requrie('mongoose');
+var mongoose = require('mongoose');
+var extend = require('mongoose-schema-extend');
 
 var relationSchema = new mongoose.Schema({
-  shapeA: ObjectId,
-  shapeB: ObjectId,
-  title: String,
-  coordsA: Array,
-  coordsB: Array
+  shapeA: {type: mongoose.Schema.ObjectId, required: true},
+  shapeB: {type: mongoose.Schema.ObjectId, required: true},
+  title: {type: String, required: true},
+  coordsA: {type: Array, required: true},
+  coordsB: {type: Array, required: true}
 });
+
+/**
+ * TODO die anderen relation typen definieren
+ */
+
+module.exports = mongoose.model('Relation', relationSchema);
