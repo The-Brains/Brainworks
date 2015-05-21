faf2a822cbc352d79c523c4b1650314f9abb3f73
Dennis Stumm <dstumm95@gmail.com>
2015-05-21 12:28:15 +0200

    Changes

--- models/diagrams/Diagram.js
+++ models/diagrams/Diagram.js
@@ -4,7 +4,10 @@
 var mongoose = require('mongoose');
 
 var diagramSchema = new mongoose.Schema({
-  name: String,
-  description: String
+  name: {type: String, required: true},
+  description: {type: String, required: true},
+  authorId: {type: mongoose.Schema.ObjectId, auto: false, required: true},
+  isPublic: {type: Boolean}
 });
 
+module.exports = mongoose.model('Diagram', diagramSchema);
