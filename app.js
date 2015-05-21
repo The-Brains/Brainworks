325c6d2bb3010ccecbfdd5a1769e527ca2a415b4
Tobias Kölpin <t.koelpin@web.de>
2015-05-21 09:07:56 +0200

    #1: New settings from the main-code

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 // var session = require('express-session');
 
-var routes = require('./routes/index');
+var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+mongoose.connect('mongodb://localhost/brainworks');
+
+var mainRoutes = require('./routes/index');
+var userRoutes = require('./routes/user');
+var diagramRoutes = require('./routes/diagram');
 var app = express();
 
 // all environments
@@ -25,13 +31,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-app.use('/', routes);
+app.use('/', mainRoutes);
+app.use('/user', userRoutes);
+app.use('/diagram', diagramRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -39,23 +47,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;
