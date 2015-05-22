ef36505dc2e2ac970f07a71556c88aba84bf1251
KyleGass <tim.moenkemoeller@web.de>
2015-05-22 11:18:42 +0200

    canvas element hinzugefügt

--- public/javascript/diagram/designer.js
+++ public/javascript/diagram/designer.js
@@ -1,21 +1,20 @@
 angular.module('brainworks.diagram')
 .controller('designerCtrl', ['$scope', function ($scope) {
   $scope.oneAtATime = true;
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'template'
-    },{
-      title: 'Dynamic Group Header - 2',
-      content: 'Dynamic Group Body - 2'
-    }
-  ];
+  $scope.groups = [{}];
 }])
 .directive('designer', function() {
   // TODO hier wird ein canvas element mit den notwendigen listeners definiert. 
   //es erhält alle shapes und relations und ruft bei diesen die zeichnen funktion auf
-  
+
 })
 .directive('designerElement', function() {
   // TODO hier wird ein canvas element mit den notwendigen listeners definiert. diese direktive wird in der seiten auswahl verndet um die einzelnen elemente zu auswahl zur verfügung zu stellen
+  return {
+    restrict:'E',
+    replace:'true',
+    template:'<canvas class="designer-element" ></canvas>'
+      
+      
+  }
 });
