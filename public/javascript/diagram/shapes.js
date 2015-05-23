1ec6f39e945d4c955e2fb0a489567f475f2df749
Tobias Kölpin <t.koelpin@web.de>
2015-05-23 21:52:33 +0200

    #1: Now the shapes!

--- public/javascript/diagram/shapes.js
+++ public/javascript/diagram/shapes.js
@@ -11,7 +11,7 @@
 var Layout = function() {};
 
 // Define a initialisation for Layout
-Layout.prototype.init = function(x, y, width, height, borderColor, lineWidth) {
+Layout.prototype.init = function(x, y, width, height, lineWidth) {
   if (typeof x === "number") this.x = x;
   else this.x = 0;
   
@@ -23,66 +23,58 @@ Layout.prototype.init = function(x, y, width, height, borderColor, lineWidth) {
   
   if (typeof height === "number") this.height = height;
   else this.height = 0;
-  
-  if (typeof borderColor === "string") this.borderColor = borderColor;
-  else this.borderColor = "black";
-  
+    
   if (typeof lineWidth === "number") this.lineWidth = lineWidth;
-  else this.lineWidth = 1;
+  else this.lineWidth = 0;
 };
 
 // Define setters for Layout
 Layout.prototype.setX = function(x) {
   if (typeof x === "number") this.x = x;
   else this.x = 0;
-}
+};
 Layout.prototype.setY = function(y) {
   if (typeof y === "number") this.y = y;
   else this.y = 0;
-}
+};
 Layout.prototype.setWidth = function(width) {
   if (typeof width === "number") this.width = width;
   else this.width = 0;
-}
+};
 Layout.prototype.setHeight = function(height) {
   if (typeof height === "number") this.height = height;
   else this.height = 0;
-}
-Layout.prototype.setBorderColor = function(borderColor) {
-  if (typeof borderColor === "string") this.borderColor = borderColor;
-  else this.borderColor = "black";
-}
+};
 Layout.prototype.setLineWidth = function(lineWidth) {
   if (typeof lineWidth === "number") this.lineWidth = lineWidth;
   else this.lineWidth = 1;
-}
+};
 
 //Define getters for Layout
 Layout.prototype.getX = function() {
   return this.x;
-}
+};
 Layout.prototype.getY = function() {
   return this.y;
-}
+};
 Layout.prototype.getWidth = function() {
   return this.width;
-}
+};
 Layout.prototype.getHeight = function() {
   return this.height;
-}
-Layout.prototype.getBorderColor = function() {
-  return this.borderColor;
-}
+};
 Layout.prototype.getLineWidth = function() {
   return this.lineWidth;
-}
+};
+
 
 /*******************************************************************************
- * The class activeClass                                                       *
+ * The class ActiveClass                                                       *
  ******************************************************************************/
 
-var ActiveClass = function(x, y, width, height, borderColor, lineWidth) {
-  this.init(x, y, width, height, borderColor, lineWidth);  
+var ActiveClass = function(x, y, width, height, lineWidth, title, fontFamily, 
+  fontSize) {
+  this.init(x, y, width, height, lineWidth, title, fontFamily, fontSize);  
 }
 // This will inherit the class Layout
 ActiveClass.prototype = new Layout();
@@ -94,38 +86,85 @@ ActiveClass.prototype.constructor = ActiveClass;
 ActiveClass.parent = Layout.prototype;
 
 // Define a initialisation for ActiveClass
-ActiveClass.prototype.init = function(x, y, width, height, borderColor, lineWidth) {
+ActiveClass.prototype.init = function(x, y, width, height, lineWidth, title, 
+  fontFamily, fontSize) {
   
   // This will add the values to the parent class
-  ActiveClass.parent.init(x, y, width, height, borderColor, lineWidth);
-}
+  ActiveClass.parent.init(x, y, width, height, lineWidth);
+  
+  if (typeof title === "string") this.title = title;
+  else this.title = "";
+  
+  if (typeof fontFamily === "string") this.fontFamily = fontFamily;
+  else this.fontFamily = "";
+  
+  if (typeof fontSize === "number") this.fontSize = fontSize;
+  else this.fontSize = 0;
+};
 
-//Define functions for DemoClass
+// Define setters for ActiveClass
+ActiveClass.prototype.setTitle = function(title) {
+  if (typeof title === "string") this.title = title;	
+};
+ActiveClass.prototype.setFontFamily = function(fontFamily) {
+  if (typeof fontFamily === "string") this.fontFamily = fontFamily;	
+};
+ActiveClass.prototype.setFontSize = function(fontSize) {
+  if (typeof fontSize === "number") this.fontSize = fontSize;	
+};
+	
+// Define getters for ActiveClass
+ActiveClass.prototype.getTitle = function() {
+  return this.title;	
+};
+ActiveClass.prototype.setFontFamily = function(fontFamily) {
+  return this.fontFamily;	
+};
+ActiveClass.prototype.setFontSize = function(fontSize) {
+  return this.fontSize;	
+};
+
+// Define functions for ActiveClass
 ActiveClass.prototype.draw = function(context) {
-  var x = DemoClass.parent.getX();
-  var y = DemoClass.parent.getY();
-  var width = DemoClass.parent.getWidth();
-  var height = DemoClass.parent.getHeight();
-  var borderColor = DemoClass.parent.getBorderColor();
-  var lineWidth = DemoClass.parent.getLineWidth();
+  var x = ActiveClass.parent.getX();
+  var y = ActiveClass.parent.getY();
+  var width = ActiveClass.parent.getWidth();
+  var height = ActiveClass.parent.getHeight();
+  var lineWidth = ActiveClass.parent.getLineWidth();
     
   var innerWidth = width * 0.8;
   var posInnerWidth = (width - innerWidth) / 2;
     
-    context.save();
-    
-    context.rect(x, y, width, height);
-    context.rect(posInnerWidth, y, innerWidth, height);
-    
-    // Settings for border color
-    if (borderColor) context.strokeStyle = borderColor; 
-    else context.strokeStyle = "black";
-    
-    // Settings for border width
-    if (lineWidth) context.lineWidth = lineWidth;
-    else context.lineWidth = 1;
+  context.save();
+  context.strokeStyle = "black";
+  
+  context.rect(x, y, width, height);
+  context.rect(posInnerWidth, y, innerWidth, height);
+      
+  // Settings for border width
+  if (lineWidth) context.lineWidth = lineWidth;
+  else context.lineWidth = 1;
+  
+  if (this.title) {
+	  
+	  // Settings for font-family  
+	  if (this.fontFamily) var fontFamily = this.fontFamily;
+	  else var fontFamily = "Arial";
+	
+	  // Settings for font-size in px
+	  if (this.fontSize) var fontSize = this.fontSize;
+	  else var fontSize = 16;
+	
+	  // Settings for text-orientation in the center of both sides
+	  var centerX = width / 2;
+	  var centerY = (height / 2) + (fontSize / 2);
+	
+	  context.font = "italic bold " + fontSize + "px " + fontFamily;
+	  context.textAlign = "center";
+	  context.fillText(this.title, centerX, centerY, width);	
+  }
               
-    context.stroke();
-    
-    context.restore();
-}
+  context.stroke();
+  
+  context.restore();
+};
