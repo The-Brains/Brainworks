5b96e1637f0fade6f9652f401f097d858fb48637
Dennis Stumm <dstumm95@gmail.com>
2015-05-24 21:08:14 +0200

    Drag and Drop auf der Canvasfläsche!

--- public/javascript/diagram/designer.js
+++ public/javascript/diagram/designer.js
@@ -12,6 +12,8 @@ angular.module('brainworks.diagram')
     template: '<canvas class="designer" height="5000px" width="5000px"></canvas>',
     link: function(scope, element, attr) {
       var selected = null;
+      var positionX = Number.MIN_VALUE;
+      var positionY = Number.MIN_VALUE;
       $(element).droppable({
         accept: '.designer-element',
         drop: function(event, ui) {
@@ -45,33 +47,30 @@ angular.module('brainworks.diagram')
         }
       };
       element.on('mousedown', function(event) {
-        result = $.grep(scope.shapes, function(shape) { return event.layerX >= shape.getX() && event.layerX <= (shape.getX() + shape.getWidth()) && event.layerY >= shape.getY() && event.layerY <= (shape.getY() + shape.getHeight()); });
+        var result = $.grep(scope.shapes, function(shape) { return event.layerX >= shape.getX() && event.layerX <= (shape.getX() + shape.getWidth()) && event.layerY >= shape.getY() && event.layerY <= (shape.getY() + shape.getHeight()); });
         selected = result[0];
+        positionX = event.layerX;
+        positionY = event.layerY;
         draw();
       });
-      element.on('click', function(event) {
-        result = $.grep(scope.shapes, function(shape) { return event.layerX >= shape.getX() && event.layerX <= (shape.getX() + shape.getWidth()) && event.layerY >= shape.getY() && event.layerY <= (shape.getY() + shape.getHeight()); });
-        if(result.length !== 0) {
-          console.log(result[0]);
-        }
-      });
       element.on('doubelclick', function(event) {
-        result = $.grep(scope.shapes, function(shape) { return event.layerX >= shape.getX() && event.layerX <= (shape.getX() + shape.getWidth()) && event.layerY >= shape.getY() && event.layerY <= (shape.getY() + shape.getHeight()); });
+        var result = $.grep(scope.shapes, function(shape) { return event.layerX >= shape.getX() && event.layerX <= (shape.getX() + shape.getWidth()) && event.layerY >= shape.getY() && event.layerY <= (shape.getY() + shape.getHeight()); });
         if(result.length !== 0) {
           console.log(result[0]);
         }
       });
       element.on('mousemove', function(event) {
-        result = $.grep(scope.shapes, function(shape) { return event.layerX >= shape.getX() && event.layerX <= (shape.getX() + shape.getWidth()) && event.layerY >= shape.getY() && event.layerY <= (shape.getY() + shape.getHeight()); });
-        if(result.length !== 0) {
-          console.log(result[0]);
+        if(angular.isDefined(selected) && selected !== null && positionX !== Number.MIN_VALUE && positionY !== Number.MIN_VALUE) {
+          selected.setX(selected.getX() + event.layerX - positionX);
+          selected.setY(selected.getY() + event.layerY - positionY);
+          positionX = event.layerX;
+          positionY = event.layerY;
         }
+        draw();
       });
       element.on('mouseup', function(event) {
-        result = $.grep(scope.shapes, function(shape) { return event.layerX >= shape.getX() && event.layerX <= (shape.getX() + shape.getWidth()) && event.layerY >= shape.getY() && event.layerY <= (shape.getY() + shape.getHeight()); });
-        if(result.length !== 0) {
-          console.log(result[0]);
-        }
+        positionX = Number.MIN_VALUE;
+        positionY = Number.MIN_VALUE;
       });
       draw();
     }
